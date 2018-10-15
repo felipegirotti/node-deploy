05fda69b8477b925a90f39955ff2439a497e2e89
Felipe Girotti <felipe.girotti@gmail.com>
2018-10-15 00:35:19 -0300

    add script of parse logs and sendmail

--- ansible/roles/hproxy/templates/haproxy.cfg.sh
+++
@@ -1,9 +0,0 @@
-SERVERS=''
-HOSTS={{ groups.apps }}
-for host in HOSTS; do
-    IP={{ hostvars[host]['ansible_host'] }}
-    SERVERS="$SERVERS \n server $host  $IP:5001"
-done
-
-sed -i "s/%servers%/$SERVERS" /etc/haproxy/haproxy.cfg
-        
